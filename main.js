22aec90bd2317494171fac2dcd487f16d8da161a
Magno Costa <magnocp123@gmail.com>
2021-07-23 22:39:55 -0300

    ajuste swiper

--- main.js
+++ main.js
@@ -32,7 +32,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // mostrar elementos quando for dando scroll
